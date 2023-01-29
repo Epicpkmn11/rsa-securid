2dfd07263617b1c3bb39847ed2779e77d30906b4
Pk11 <epicpkmn11@outlook.com>
2023-01-29 12:43:04 -0600

    Add support for other token types

--- src/index.ts
+++ src/index.ts
@@ -38,6 +38,11 @@ export interface Code {
 
 if(typeof document !== "undefined") {
     const input = document.getElementById("input") as HTMLInputElement;
+    const devId = document.getElementById("devid") as HTMLInputElement;
+    const devIdDiv = document.getElementById("devid-div") as HTMLElement;
+    const password = document.getElementById("password") as HTMLInputElement;
+    const passwordDiv = document.getElementById("password-div") as HTMLElement;
+    const version = document.getElementById("version") as HTMLOptionElement;
     const pin = document.getElementById("pin") as HTMLInputElement;
     const remember = document.getElementById("remember") as HTMLInputElement;
     const output = document.getElementById("output") as HTMLInputElement;
@@ -54,16 +59,69 @@ if(typeof document !== "undefined") {
     }
 
     let setToken = function() {
+        // Token
         try {
-            token = v4(input.value);
+            switch(version.value) {
+                case "2":
+                    token = v2(input.value, password.value, devId.value);
+                    break;
+                case "3":
+                    token = v3(input.value, password.value, devId.value);
+                    break;
+                case "4":
+                default:
+                    token = v4(input.value, password.value, devId.value);
+                    break;
+            }
             input.classList.add("valid");
+            devId.classList.add("valid");
+            password.classList.add("valid");
+
+            if(token.flags.deviceIdIsRequired)
+                devIdDiv.classList.remove("hidden");
+            else
+                devIdDiv.classList.add("hidden");
+
+            if(token.flags.passwordIsRequired)
+                passwordDiv.classList.remove("hidden");
+            else
+                passwordDiv.classList.add("hidden");
         } catch(e) {
+            console.log("Error loading token", e);
             token = null;
             input.classList.remove("valid");
+            if(e.message.includes("devid_hash") || e.message.includes("device ID of length")) {
+                devId.classList.remove("valid");
+                devIdDiv.classList.remove("hidden");
+            }
+            if(e.message.includes("pass_hash") || e.message.includes("decryptedSeedHash")) {
+                password.classList.remove("valid");
+                passwordDiv.classList.remove("hidden");
+            }
+
+            if(e.message == "Missing deviceId")
+                devIdDiv.classList.remove("hidden");
+            if(e.message == "Missing password")
+                passwordDiv.classList.remove("hidden");
+                
+            if(e.message == "Token too short") {
+                devIdDiv.classList.add("hidden");
+                passwordDiv.classList.add("hidden");
+            }
         }
-        
-        if(remember.checked)
+
+        // PIN
+        if(pin.value.match(/^\d{4,8}$/))
+            pin.classList.add("valid");
+        else
+            pin.classList.remove("valid");
+
+        if(remember.checked) {
             localStorage.token = input.value;
+            localStorage.devId = devId.value;
+            localStorage.password = password.value;
+            localStorage.version = version.value;
+        }
 
         updateOutput();
     }
@@ -75,21 +133,23 @@ if(typeof document !== "undefined") {
     }, 1000);
     
     input.addEventListener("change", setToken);
-
-    pin.addEventListener("change", function() {
-        if(pin.value.length >= 4 && pin.value.length <= 8)
-            pin.classList.add("valid");
-        else
-            pin.classList.remove("valid");
-
-        updateOutput();
-    });
+    devId.addEventListener("change", setToken);
+    password.addEventListener("change", setToken);
+    version.addEventListener("change", setToken);
+    pin.addEventListener("change", setToken);
 
     remember.addEventListener("change", function() {
-        if(remember.checked)
+        if(remember.checked) {
             localStorage.token = input.value;
-        else
+            localStorage.devId = devId.value;
+            localStorage.password = password.value;
+            localStorage.version = version.value;
+        } else {
             delete localStorage.token;
+            delete localStorage.devId;
+            delete localStorage.password;
+            delete localStorage.version;
+        }
     });
 
     let searchParams = new URLSearchParams(window.location.search);
@@ -104,6 +164,12 @@ if(typeof document !== "undefined") {
         window.history.replaceState(null, "", window.location.pathname + searchStr + window.location.hash);
     } else if(localStorage.token) {
         input.value = localStorage.token;
+        if(localStorage.devId)
+            devId.value = localStorage.devId;
+        if(localStorage.password)
+            password.value = localStorage.password;
+        if(localStorage.version)
+            version.value = localStorage.version;
         setToken();
         remember.checked = true;
     }
